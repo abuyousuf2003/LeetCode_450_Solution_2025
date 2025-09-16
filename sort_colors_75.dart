27d90d31845d36a6bd24660a2f384e5222994011
Abu Yousuf <abuyousufmdjumman@gmail.com>
2025-09-16 21:33:37 +0600

    Sorted the color

---
+++ sort_colors_75.dart
@@ -0,0 +1,14 @@
+class sortColorAlgorithm{
+void sortColor(List nums)
+{
+nums.sort();
+
+}
+void main() {
+ sortColorAlgorithm obj= sortColorAlgorithm
+ ();
+  obj.sortColor([2,0,2,1,1,0]);
+
+
+
+}}
